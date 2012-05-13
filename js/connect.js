0480db68430852b9b006d82031b9de79ba6d89cf
Martin Halamicek <martin@halamicek.cz>
2012-05-13 11:14:46 +0200

    Connect page styling

--- js/connect.js
+++ js/connect.js
@@ -5,12 +5,7 @@ jQuery(function($) {
             chrome.tabs.remove( tab.id );
         });
 	}, onAuthorize = function() {
-		$("#output").empty();
-		
-		Trello.members.get("me", function(member){
-			$("#output").append("Thank you, you're now authenticated you can start using Trello My Cards extension immediately. <br/>This page will be automatically closed in few seconds.");
-		});
-
+		$("body").addClass("authorized");
 		setTimeout(closeCurrentTab, 10000);
 	};
 						  
